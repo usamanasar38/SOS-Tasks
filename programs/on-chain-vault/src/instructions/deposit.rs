32b25304b2c6604a4eb0688948a75891b3e8cf19
Usama Nasar <usamanasar38@gmail.com>
2025-08-05 23:53:19 +0500

    check if valut is locked

--- programs/on-chain-vault/src/instructions/deposit.rs
+++ programs/on-chain-vault/src/instructions/deposit.rs
@@ -11,6 +11,7 @@
 ///-------------------------------------------------------------------------------
 
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::native_token::LAMPORTS_PER_SOL;
 use anchor_lang::solana_program::program::invoke;
 use anchor_lang::solana_program::system_instruction::transfer;
 use crate::state::Vault;
@@ -19,11 +20,33 @@ use crate::events::DepositEvent;
 
 #[derive(Accounts)]
 pub struct Deposit<'info> {
-    // TODO: Add required accounts and constraints
-    pub placeholder: Signer<'info>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        init, 
+        payer = user, 
+        // space = discriminant + account size
+        space = 8 + Vault::INIT_SPACE,
+        seeds = [b"vault", user.key().as_ref()],
+        bump
+    )]
+    pub vault: Account<'info, Vault>,
+    pub system_program: Program<'info, System>,
 }
 
 pub fn _deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
-    // TODO: Implement deposit functionality
-    todo!()
+    let user = &mut ctx.accounts.user;
+    let balance = user.lamports / LAMPORTS_PER_SOL;
+
+    if ctx.accounts.vault.locked {
+        return Err(VaultError::VaultLocked.into());
+    }
+
+
+    emit!(DepositEvent {
+        user: ctx.accounts.user.key(),
+        vault: ctx.accounts.vault.key(),
+        amount,
+    });
+    Ok(())
 }
