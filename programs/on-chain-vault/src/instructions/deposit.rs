94a3de87190184a80bc913e6560931831de968a7
Usama Nasar <usamanasar38@gmail.com>
2025-08-05 23:54:38 +0500

    check if user have enough balance

--- programs/on-chain-vault/src/instructions/deposit.rs
+++ programs/on-chain-vault/src/instructions/deposit.rs
@@ -20,13 +20,13 @@ use crate::events::DepositEvent;
 #[derive(Accounts)]
 pub struct Deposit<'info> {
     #[account(mut)]
-    pub signer: Signer<'info>,
+    pub user: Signer<'info>,
     #[account(
         init, 
-        payer = signer, 
+        payer = user, 
         // space = discriminant + account size
         space = 8 + Vault::INIT_SPACE,
-        seeds = [b"vault", signer.key().as_ref()],
+        seeds = [b"vault", user.key().as_ref()],
         bump
     )]
     pub vault: Account<'info, Vault>,
@@ -34,14 +34,18 @@ pub struct Deposit<'info> {
 }
 
 pub fn _deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+    let user = &ctx.accounts.user;
     let vault = &ctx.accounts.vault;
+
+    // Check if the user has enough balance to deposit
+    require!(user.lamports() >= amount, VaultError::VaultLocked);
     
     // Check if vault is locked
     require!(!vault.locked, VaultError::VaultLocked);
 
 
     emit!(DepositEvent {
-        user: ctx.accounts.signer.key(),
+        user: ctx.accounts.user.key(),
         vault: ctx.accounts.vault.key(),
         amount,
     });
